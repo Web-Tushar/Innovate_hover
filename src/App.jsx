523d2818853c1c6d680225788a3f063590be4c79
Tushar <153347682+Web-Tushar@users.noreply.github.com>
2025-01-04 16:40:33 +0600

    Experience part add

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navber from './component/navber/Navber'
 import Banner from './component/banner/Banner'
 import Service from './component/service_part/Service'
+import Experience from './component/experience_part/Experience'
 
 const App = () => {
   return (
     <div>
       <Navber />
-      <Banner/>
-      <Service/>
+      <Banner />
+      <Service />
+      <Experience />
     </div>
   )
 }
---
+++ src/component/experience_part/Experience.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Experience = () => {
+     return (
+          <div>
+               <section id="experience">
+                    <div class="container">
+                         <div class="experience_heading_group">
+                              <h4 class="sub_heading">digital experience</h4>
+                              <h2 class="heading">Connect people in digital life</h2>
+
+                         </div>
+                    </div>
+                    <div class="experience_wrapper">
+                         <div class="experience_img">
+                              <div class="experience_img_box">
+                                   <img src="./images/vincent-van-zalinge-OxXvx41w3Hc-unsplash.jpg" alt="no_found"/>
+                              </div>
+
+                         </div>
+                         <div class="experience_content">
+                              <h2>More than just an ad agency, we harness the tools of traditional and digital.</h2>
+                              <p class="paragraph">With over 25 years of experience, we have crafted thousands of strategic discovery process that enables us to peel back the layers which enable us to understand, connect, represent and dominate your market.</p>
+                              <a class="common_btn" href="#">Cotact us</a>
+                              <div class="experience_shape">
+                                   <img src="./images/experi_shape.png" alt="no_found"/>
+                              </div>
+
+                         </div>
+
+
+                    </div>
+
+               </section>
+          </div>
+     )
+}
+
+export default Experience
